3dfedb43a8c16d07e6fc7a360194d31243934723
ArielM1225 <ariemeneses243@gmail.com>
2024-06-05 00:16:23 -0300

    Agregar archivos de proyecto.

---
+++ Meneses_CRUD/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meneses_CRUD.Models
+{
+    public partial class Cliente
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? NumTel { get; set; }
+    }
+}
